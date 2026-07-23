700b612667a0dc4850fd2f688e1b082623794ba8
microbiteindia <microbiteindia@gmail.com>
2026-07-23 18:02:22 +0530

    Fix puppeteer chrome executable path

--- puppeteer.config.cjs
+++
@@ -1,9 +0,0 @@
-const { resolve } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Uses absolute path to ensure extract-zip receives a valid target directory
-  cacheDirectory: resolve(__dirname, '.cache', 'puppeteer'),
-};
